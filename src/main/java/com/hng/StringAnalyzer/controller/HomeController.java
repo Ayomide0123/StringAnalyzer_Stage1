940d78dd833b9e67efdd83f44b7fd450498b3ebc
Ayomide <oyetimehin31@gmail.com>
2025-10-21 14:11:40 +0100

    docs: modify HomeController

--- src/main/java/com/hng/StringAnalyzer/controller/HomeController.java
+++ src/main/java/com/hng/StringAnalyzer/controller/HomeController.java
@@ -1,15 +1,15 @@
 package com.hng.StringAnalyzer.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/")
 public class HomeController {
 
     @GetMapping
     public String sayHello() {
-        return "Hello World";
+        return "index";
     }
 }
